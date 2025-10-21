e5d15c38711b16a29a63ce38964fbb0abd75f785
Ludwin <ludwin.jhurgo@gmail.com>
2025-10-20 21:07:44 -0500

    delete dist

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(path.join(__dirname, "dist")));
 
 // Redirigir todo al index.html (Single Page App)
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.listen(port, () => {
