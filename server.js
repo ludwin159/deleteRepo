bd28ef1c47f7080f5201734830de5d95eb776ad4
Ludwin <ludwin.jhurgo@gmail.com>
2025-10-20 21:04:13 -0500

    server

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+// Carpeta estática (ajusta 'dist' o 'build' según tu framework)
+app.use(express.static(path.join(__dirname, "dist")));
+
+// Redirigir todo al index.html (Single Page App)
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
+app.listen(port, () => {
+    console.log(`Servidor ejecutándose en puerto ${port}`);
+});
